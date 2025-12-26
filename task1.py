f3f82af76c1bb3cd54afaad3da8aa0ce68c926e8
Arvind Kumar <arvindk88641@gmail.com>
2025-12-26 15:55:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+# 1. A program that checks if a given number is even or odd.
+number = int(input("Enter The Number: "))
+if number % 2 == 0:
+    print(number, "is an even numbers")
+else:
+    print(number, "is an odd number")
---
+++ task2.py
@@ -0,0 +1,7 @@
+# # Loop from 1 to 50
+total = 0
+
+for i in range(1, 51):
+    total = total + i
+
+print("The sum of numbers from 1 to 50 is:", total)
